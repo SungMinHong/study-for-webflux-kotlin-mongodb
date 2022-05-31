f0ca83bd55327e46261db5fd2fd50116d495f580
홍성민(SungMin Hong)/Platform Engineering팀/11ST <devmonster@11stcorp.com>
2022-05-31 11:04:23 +0900

    init

---
+++ src/main/kotlin/com/study/studywebfluxkotlinmongo/StudyWebfluxKotlinMongoApplication.kt
@@ -0,0 +1,11 @@
+package com.study.studywebfluxkotlinmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyWebfluxKotlinMongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyWebfluxKotlinMongoApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/studywebfluxkotlinmongo/StudyWebfluxKotlinMongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.studywebfluxkotlinmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyWebfluxKotlinMongoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
