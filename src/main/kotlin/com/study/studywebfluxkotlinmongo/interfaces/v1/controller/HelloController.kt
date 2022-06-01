bc338c17e11a35aa187d47f5b05973ce80c97648
홍성민(SungMin Hong)/Platform Engineering팀/11ST <devmonster@11stcorp.com>
2022-06-01 22:00:29 +0900

    반환형으로 mono 사용

--- src/main/kotlin/com/study/studywebfluxkotlinmongo/interfaces/v1/controller/HelloController.kt
+++ src/main/kotlin/com/study/studywebfluxkotlinmongo/interfaces/v1/controller/HelloController.kt
@@ -4,12 +4,13 @@ import com.study.studywebfluxkotlinmongo.interfaces.v1.dto.GetHelloDTO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RequestMapping("/v1/hello")
 @RestController
 class HelloController {
     @GetMapping
-    fun getHelloWorld(): GetHelloDTO {
-        return GetHelloDTO(1, "Hello world!")
+    fun getHelloWorld(): Mono<GetHelloDTO> {
+        return Mono.just(GetHelloDTO(1, "Hello world!"))
     }
 }
