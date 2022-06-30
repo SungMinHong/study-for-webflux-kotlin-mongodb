d152cf2fa1cf811be44784577b5948a9887bf1d5
SungMin Hong <honggasm@gmail.com>
2022-06-30 10:55:01 +0900

    chapter2.4: 명시적으로 타입 변환하기 예제

---
+++ src/test/kotlin/cases/Chapter2o4.kt
@@ -0,0 +1,23 @@
+package cases
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+
+internal class Chapter2o4 : StringSpec({
+    /* 사용 가능한 타입 변환 메소드
+     * toLong()
+     * toByte()
+     * toChar()
+     * toShort()
+     * toInt()
+     * toFloat()
+     * toDouble()
+     */
+    "2-24.코틀린에서 Int를 Long으로 승격시키기" {
+        val intVar: Int = 3
+//        val longVar: Long = intVar  // 컴파일되지 않음
+        val longVar = intVar.toLong() // 명시적 타입 변환
+        longVar.shouldBeInstanceOf<Long>()
+    }
+})
