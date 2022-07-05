19bfacb367828d3ef1b2a174ca781eef0d5f8b65
SungMin Hong <honggasm@gmail.com>
2022-07-05 22:14:08 +0900

    chapter3.6: 나중 초기화를 위해 lateinit 사용하기

---
+++ src/test/kotlin/chapter3/chapter3o6/Chapter3o5.kt
@@ -0,0 +1,9 @@
+package chapter3.chapter3o2
+
+import io.kotest.core.spec.style.FreeSpec
+
+internal class Chapter3o5 : FreeSpec({
+    /** 나중 초기화를 위해 lateinit 사용하기
+     * 이 테크닉은 꼭 필요한 경우만 사용하자. 가능하다면 지연 평가 같은 대안을 먼저 고려하자.
+     */
+})
