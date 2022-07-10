b417f385ce31d5e613fd5c87e2adb7f35c21be12
SungMin Hong <honggasm@gmail.com>
2022-07-10 16:36:39 +0900

    reactor core 에러, 완료 처리 예제

--- src/test/java/reactor/FluxMonoSimpleTest.java
+++ src/test/java/reactor/FluxMonoSimpleTest.java
@@ -37,6 +37,29 @@ class FluxMonoSimpleTest {
             ints.subscribe(System.out::println);
             ints.subscribe(System.out::println);
         }
+
+        @Test
+        void 에러_처리() {
+            Flux<Integer> ints = Flux.range(1, 4) // 구독자가 생기면 값 4개를 생성하는 Flux를 세팅한다
+                    .map(i -> {
+                        if (i <= 3) {
+                            return i;   // 1~3은 값을 리턴
+                        }
+                        throw new RuntimeException("Got to 4"); // 예외 반환
+                    });
+            ints.subscribe(System.out::println, // 출력
+                    error -> System.err.println("Error: " + error)  // 반환딘 예외 출력
+            );
+        }
+
+        @Test
+        void 완료_처리() {
+            Flux<Integer> ints = Flux.range(1, 4); // 구독자가 생기면 값 4개를 생성하는 Flux를 세팅한다
+            ints.subscribe(System.out::println,
+                    error -> System.err.println("Error " + error),
+                    () -> System.out.println("Done(완료)")); // 완료 이벤트 핸들러를 가지고 있는 구독자로 구독한다.
+            // 에러 신호와 완료 신호는 모두 종료 이벤트이며, 상호 배타적이다(둘다 받을 수는 없다). 완료 컨슈머가 잘 동작하려면 에러가 발생하지 않도록 주의해야 한다.
+        }
     }
 
 }
